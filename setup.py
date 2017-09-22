86f4572b1ec5efb5465c7098159f15de091af1fc
Nikita <iamlilacpenguin@gmail.com>
2017-09-22 11:26:24 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='simple_drmaa_scheduler',
-    version='0.0.6',
+    version='0.0.7',
     packages=['scheduler', 'scheduler.parser', 'scheduler.executor'],
     url='',
     license='',
